563450280cb046b46ecccb9a789133badfe828a2
Java02 <alekseenkooleg2004@gmail.com>
2021-03-13 12:32:30 +0200

    Initial commit

---
+++ src/pasha/edu/Main.java
@@ -0,0 +1,30 @@
+package pasha.edu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] names = new String[8];
+	names [0] = "Паша";
+	names [1] = "Артем";
+	names [2] = "Деник";
+	names [3] = "Олег";
+	names [4] = "Девид";
+	names [5] = "Саша";
+	names [6] = "Максим 1";
+	names [7] = "Максим 2";
+
+        String[] figures = new String[12];
+        figures [0] = "Куб";
+        figures [1] = "Прямокутний Трикутник";
+        figures [2] = "Ромб";
+        figures [3] = "Паралелаграm";
+        figures [4] = "Рівностороній Трикутник";
+        figures [5] = "Пралепіпет";
+        figures [6] = "Трапеція";
+        figures [7] = "Круг";
+        figures [8] = "Еліпс";
+        figures [9] = "Циліндр";
+        figures [10] = "Конус";
+        figures [11] = "Піраміда";
+    }
+}
